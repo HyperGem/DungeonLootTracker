c6491b4ee7b4c7815f6fd7bb120cf347ca44dabb
HyperGem <32209976+HyperGem@users.noreply.github.com>
2019-12-22 14:09:43 -0500

    Update Core.lua
    Main Frame has been created as well as Button and Slider template created.

--- Core.lua
+++ Core.lua
@@ -14,4 +14,44 @@ for i = 1, NUM_CHAT_WINDOWS do
 end
 
 ------------------------------------------------------------------
+-- Main Parent Frame is Created where all the other Frames and Elements will inherit from.
+local UIConfig = CreateFrame("Frame", "DLT_MainFrame", UIParent, "BasicFrameTemplateWithInset")
+UIConfig:SetSize(360,420);
+UIConfig:SetPoint("CENTER", UIParent, "CENTER");
+
+UIConfig.title = UIConfig:CreateFontString(nil, "OVERLAY");
+UIConfig.title:SetFontObject("GameFontHighlight");
+
+UIConfig.title:SetPoint("Left", UIConfig.TitleBg, "Left", 5, 0);
+UIConfig.title:SetText("Dungeon Loot Tracker");
+
+
+
+-- Buttons for the Main Frame
+UIConfig.closeBtn = CreateFrame("Button", nil, UIConfig, "GameMenuButtonTemplate");
+UIConfig.closeBtn:SetPoint("CENTER", UIConfig, "Bottom", 0, 30);
+UIConfig.closeBtn:SetSize(60, 30);
+UIConfig.closeBtn:SetText("Close");
+UIConfig.closeBtn:SetNormalFontObject("GameFontNormalLarge");
+UIConfig.closeBtn:SetHighlightFontObject("GameFontHighlightLarge");
+
+-- Potential Sliders for the main frame.
+
+UIConfig.sliderBtn = CreateFrame("Slider", nil, UIConfig, "OptionsSliderTemplate");
+UIConfig.sliderBtn:SetPoint("CENTER", UIConfig, "CENTER", 0, 135);
+UIConfig.sliderBtn:SetMinMaxValues(10, 50);
+UIConfig.sliderBtn:SetValue(20);
+UIConfig.sliderBtn:SetValueStep(10);
+UIConfig.sliderBtn:SetObeyStepOnDrag(true);
+
+
+-- Event handlers to be added later.
+
+--local function eventHandlerCloseBtn(event e)
+	--left blank intentionally
+--end
+
+
+
+-- Left for later to be added.
 
